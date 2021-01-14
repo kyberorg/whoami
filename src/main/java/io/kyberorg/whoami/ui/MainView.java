78071c3d449993bfde879d3306ffabe8a7a5db0f
Alex Muravya <root@kyberorg.io>
2021-01-14 12:52:24 +0200

    Icons and stuff. More stuff.

--- src/main/java/io/kyberorg/whoami/ui/MainView.java
+++ src/main/java/io/kyberorg/whoami/ui/MainView.java
@@ -45,14 +45,14 @@ public class MainView extends VerticalLayout implements PageConfigurator {
 
     @Override
     public void configurePage(InitialPageSettings settings) {
-        /*settings.addFavIcon("icon", "/icons/favicon-32x32.png", "32x32");
+        settings.addFavIcon("icon", "/icons/favicon-32x32.png", "32x32");
         settings.addLink("shortcut icon", "/icons/favicon-16x16.png");
         settings.addLink("apple-touch-icon", "/icons/apple-touch-icon.png");
         settings.addLink("manifest", "/site.webmanifest");
-        settings.addLink("mask-icon", "/icons/safari-pinned-tab.svg");*/
+        settings.addLink("mask-icon", "/icons/safari-pinned-tab.svg");
 
-        settings.addMetaTag("apple-mobile-web-app-title", "whoami");
-        settings.addMetaTag("application-name", "whoami");
+        settings.addMetaTag("apple-mobile-web-app-title", "WhoAmI");
+        settings.addMetaTag("application-name", "WhoAmI");
         settings.addMetaTag("msapplication-TileColor", "#ffc40d");
         settings.addMetaTag("theme-color", "#ffffff");
 
